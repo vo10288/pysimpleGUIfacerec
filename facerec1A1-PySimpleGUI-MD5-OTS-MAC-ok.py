141c2854bc738d2c78dcfa04331c3d183da011d4
Visi@n <broi.antonio@gmail.com>
2024-04-21 23:38:46 +0200

    Add files via upload

---
+++ facerec1A1-PySimpleGUI-MD5-OTS-MAC-ok.py
@@ -0,0 +1,322 @@
+import os
+import cv2
+import numpy as np
+import face_recognition
+import PySimpleGUI as sg
+from datetime import datetime
+from io import BytesIO
+import hashlib
+import subprocess
+import pdfdocument
+
+def calculate_hash(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        file_hash = hashlib.sha256(data).digest()
+        return file_hash
+
+
+def opentimestampsClien(file_path):
+	# Calcola l'hash del file
+	file_hash = calculate_hash(file_path)
+
+	# Crea un oggetto `OpentimestampsClient`
+	client = otsclient.create_client()
+
+	try:
+		# Creazione di un timestamp
+		timestamp = client.stamp(file_hash)
+
+		# Stampa l'hash originale e l'hash del timestamp sulla blockchain
+		print("Hash originale: ", file_hash.hex())
+		print("Hash del timestamp: ", timestamp.msg.hex())
+		
+		# Salvataggio del report
+		report_path = file_path + ".ots"
+		with open(report_path, 'wb') as report_file:
+			report_file.write(timestamp.serialize())
+
+		# Verifica che il timestamp sia valido
+		verification = client.verify(timestamp)
+		if verification:
+			print("Il timestamp è valido!")
+		else:
+			print("Il timestamp non è valido.")
+	except otsclient.exceptions.TimeoutError:
+		print("Timeout durante la creazione del timestamp.")
+	except otsclient.exceptions.Error as e:
+		print("Errore durante la creazione del timestamp:", str(e))
+	
+	return report_file_ots
+
+def md5Checksum(filePath,url):
+    if url==None:
+        with open(filePath, 'rb') as fh:
+            m = hashlib.md5()
+            while True:
+                data = fh.read(8192)
+                if not data:
+                    break
+                m.update(data)
+            return m.hexdigest()
+    else:
+        r = requests.get(url, stream=True)
+        m = hashlib.md5()
+        for line in r.iter_lines():
+            m.update(line)
+        return m.hexdigest()
+# Funzione per il riconoscimento facciale tra due immagini
+def perform_face_recognition(image_1, image_2):
+    # Converte le immagini in RGB (per il riconoscimento facciale di face_recognition)
+	image_1_rgb = cv2.cvtColor(image_1, cv2.COLOR_BGR2RGB)
+	image_2_rgb = cv2.cvtColor(image_2, cv2.COLOR_BGR2RGB)
+
+    # Trova i volti nelle immagini
+	face_locations_1 = face_recognition.face_locations(image_1_rgb)
+	face_locations_2 = face_recognition.face_locations(image_2_rgb)
+
+	if len(face_locations_1) > 0 and len(face_locations_2) > 0:
+        # Calcola i landmark del viso
+		face_landmarks_1 = face_recognition.face_landmarks(image_1, face_locations_1)
+		face_landmarks_2 = face_recognition.face_landmarks(image_2, face_locations_2)
+
+        # Disegna i keypoint, i landmark e il quadrato di corrispondenza sulle immagini
+		draw_keypoints(image_1, face_landmarks_1[0])
+		draw_keypoints(image_2, face_landmarks_2[0])
+        
+        
+         # Disegna il quadrato di corrispondenza solo se ci sono volti trovati
+		draw_match_rectangle(image_1, face_locations_1[0], image_2, face_locations_2[0])
+        
+		first_image_encodeds = face_recognition.face_encodings(image_1, face_locations_1)
+		second_image_encodeds = face_recognition.face_encodings(image_2, face_locations_2)[0]
+        
+        
+        # Esegue il riconoscimento facciale tra le immagini non ridimensionate
+		matches = face_recognition.compare_faces(first_image_encodeds, second_image_encodeds)
+		distances = face_recognition.face_distance(first_image_encodeds, second_image_encodeds)
+
+    # Restituisce il risultato del confronto e le immagini modificate
+	return first_image_encodeds, second_image_encodeds, face_landmarks_1, face_landmarks_1, face_locations_1, face_locations_2, matches, distances, image_1, image_2
+
+    
+# Funzione per disegnare i keypoint e i landmark sulle immagini
+def draw_keypoints(image, landmarks):
+    for keypoint in landmarks:
+        for point in landmarks[keypoint]:
+            cv2.circle(image, point, 2, (255, 0, 0), -1)
+
+# Funzione per disegnare il quadrato di corrispondenza tra i volti sulle immagini
+def draw_match_rectangle(image_1, face_location_1, image_2, face_location_2):
+    top_1, right_1, bottom_1, left_1 = face_location_1
+    top_2, right_2, bottom_2, left_2 = face_location_2
+
+    cv2.rectangle(image_1, (left_1, top_1), (right_1, bottom_1), (0, 255, 255), 2)
+    cv2.rectangle(image_2, (left_2, top_2), (right_2, bottom_2), (0, 255, 255), 2)
+
+# Crea una cartella "reports" se non esiste già
+if not os.path.exists("reports"):
+    os.makedirs("reports")
+    
+if not os.path.exists("targets"):
+    os.makedirs("targets")
+    
+
+# Crea una cartella "temp" per le immagini modificate temporanee
+if not os.path.exists("temp"):
+    os.makedirs("temp")
+
+font_size = 26
+background_color = 'light gray'
+text_color = 'darkblue'
+
+
+# Crea l'interfaccia grafica
+layout = [
+    [sg.Image(key='image_left1'),sg.Image(key='image_left'), sg.Image(key='image_right1'), sg.Image(key='image_right')],
+    [sg.Button('Seleziona immagine sinistra', button_color=('white', 'darkblue'), size=(25, 1)), sg.Button('Seleziona immagine destra', button_color=('white', 'darkblue'), size=(25, 1)), sg.Button('Face Recognition', button_color=('white', 'darkblue'), size=(20, 1)), sg.Button('OpenTimestamps-Client', button_color=('white', 'darkblue'), size=(25, 1)), sg.Button('Stampa finestra', button_color=('white', 'darkblue'), size=(15, 1))],
+    [sg.Output(size=(90, 20), key='output', font=('Helvetica', font_size), background_color=background_color, text_color=text_color)]
+]
+
+window = sg.Window('Face Recognition', layout)
+
+image_1 = None
+image_2 = None
+
+# Ciclo dell'interfaccia grafica
+while True:
+	event, values = window.read()
+
+	if event == sg.WINDOW_CLOSED:
+		break
+	elif event == 'Seleziona immagine sinistra':
+		image_path_1 = sg.popup_get_file('Seleziona l\'immagine sinistra')
+		try:
+			
+			if image_path_1:
+            
+				# Carica l'immagine
+				resized_image1 = cv2.imread(image_path_1)
+
+				# Ottieni la larghezza e l'altezza dell'immagine
+				height, width = resized_image1.shape[:2]
+
+				# Controlla se la larghezza o l'altezza sono superiori a 500px
+				if width > 230 or height > 230:
+					# Calcola il fattore di ridimensionamento in base alla dimensione massima desiderata (500px)
+					scale = max(230 / width, 230 / height)
+
+					# Calcola le nuove dimensioni mantenendo le proporzioni
+					new_width = int(width * scale)
+					new_height = int(height * scale)
+
+					# Ridimensiona l'immagine utilizzando le nuove dimensioni
+					image_1 = cv2.resize(resized_image1, (new_width, new_height))
+
+					# Ora l'immagine ridimensionata è memorizzata nella variabile 'resized_image'
+				else:
+					# L'immagine non ha bisogno di essere ridimensionata
+					image_1 = resized_image1
+
+					# Continua ad utilizzare 'resized_image' come necessario
+
+				if image_1 is not None:
+					image_11_bytes = cv2.imencode('.png', image_1)[1].tobytes()
+					window['image_left1'].update(data=image_11_bytes)
+					
+					image_1_rgb = cv2.cvtColor(image_1, cv2.COLOR_BGR2RGB)
+					face_landmarks_1 = face_recognition.face_landmarks(image_1_rgb)
+					draw_keypoints(image_1, face_landmarks_1[0])
+					image_1_bytes = cv2.imencode('.png', image_1)[1].tobytes()
+					window['image_left'].update(data=image_1_bytes)
+					
+		except:
+			print('image on the left not reloaded!!')
+						
+	elif event == 'Seleziona immagine destra':
+		
+		image_path_2 = sg.popup_get_file('Seleziona l\'immagine destra')
+		try:
+			
+			if image_path_2:
+				# Carica l'immagine
+				resized_image = cv2.imread(image_path_2)
+
+				# Ottieni la larghezza e l'altezza dell'immagine
+				height, width = resized_image.shape[:2]
+
+				# Controlla se la larghezza o l'altezza sono superiori a 500px
+				if width > 230 or height > 230:
+					# Calcola il fattore di ridimensionamento in base alla dimensione massima desiderata (500px)
+					scale = max(230 / width, 230 / height)
+
+					# Calcola le nuove dimensioni mantenendo le proporzioni
+					new_width = int(width * scale)
+					new_height = int(height * scale)
+
+					# Ridimensiona l'immagine utilizzando le nuove dimensioni
+					image_2 = cv2.resize(resized_image, (new_width, new_height))
+
+					# Ora l'immagine ridimensionata è memorizzata nella variabile 'resized_image'
+				else:
+					# L'immagine non ha bisogno di essere ridimensionata
+					image_2 = resized_image
+
+				if image_2 is not None:
+					image_22_bytes = cv2.imencode('.png', image_2)[1].tobytes()
+					window['image_right1'].update(data=image_22_bytes)
+					
+					image_2_rgb = cv2.cvtColor(image_2, cv2.COLOR_BGR2RGB)
+					face_landmarks_2 = face_recognition.face_landmarks(image_2_rgb)
+					draw_keypoints(image_2, face_landmarks_2[0])
+					image_2_bytes = cv2.imencode('.png', image_2)[1].tobytes()
+					window['image_right'].update(data=image_2_bytes)
+					
+		except:
+			print('image on the right not reloaded!!')
+			
+	elif event == 'Face Recognition':
+		if image_1 is not None and image_2 is not None:
+			try:
+                # Esegue il riconoscimento facciale tra le due immagini e ottiene il risultato del confronto
+				first_image_encodeds, second_image_encodeds, face_landmarks_1, face_landmarks_1, face_locations_1, face_locations_2, matches, distances, image_1, image_2 = perform_face_recognition(image_1, image_2)
+
+                # Mostra il risultato del confronto
+				if matches is not None:
+					print('Risultato del confronto facciale:')
+					print(matches)
+					distances = ((1-distances)*100)
+					print(str(distances)+' %')
+					
+					
+					image_1_bytes = cv2.imencode('.png', image_1)[1].tobytes()
+					window['image_left'].update(data=image_1_bytes)
+					
+					image_2_bytes = cv2.imencode('.png', image_2)[1].tobytes()
+					window['image_right'].update(data=image_2_bytes)
+				else:
+					print('Nessun volto trovato nelle immagini.')
+
+                # Salva il risultato in un file CSV
+				timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+				report_file = f'reports/{timestamp}.csv'
+				
+				hash_image_1 = md5Checksum(image_path_1, None)
+				
+				hash_image_2 = md5Checksum(image_path_2, None)
+				
+				with open(report_file, 'w') as file:
+					file.write('Immagine 1,Hash-md5-immagine1,Immagine 2,Hash-md5-immagine2,Risultato,Percentuale di compatibilità biometrica\n')
+					file.write(f'{image_path_1},{hash_image_1},{image_path_2},{hash_image_2},{matches},str({distances} %)')
+                    
+					file.write(f'FACE LOCATION FIRST IMAGE SX\n')
+					file.write(f'str({face_locations_1})\n')
+                    
+					file.write(f'FACE LANDMARKS FIRST IMAGE SX\n')
+					file.write(f'str({[face_landmarks_1]})\n')
+                    
+					file.write(f'FACE ENCODING FIRST IMAGE SX\n')
+					file.write(f'str({first_image_encodeds})\n')
+                    
+					file.write(f'FACE LOCATION SECOND IMAGE DX\n')
+					file.write(f'str({face_locations_2})\n')
+                    
+					file.write(f'FACE LANDMARKS SECOND IMAGE SX\n')
+					file.write(f'str({face_landmarks_2})\n')
+                    
+					file.write(f'FACE ENCODING SECOND IMAGE DX\n')
+					file.write(f'str({second_image_encodeds})\n')
+
+				print(f'Risultati salvati in {report_file}')
+				hash_report = md5Checksum(report_file, None)
+				print(f'il report ha il seguente MD5:\n {hash_report}')
+				print(f'la prima immagine ha il seguente MD5:\n {hash_image_1} ')
+				print(f'la seconda immagine ha il seguente MD5:\n {hash_image_1} ')
+				
+			except Exception as e:
+				print(f"Errore durante il riconoscimento facciale: {str(e)}")
+		else:
+			print('Seleziona entrambe le immagini prima di eseguire il confronto facciale')
+			
+	elif event == 'OpenTimestamps-Client':
+		
+		command = (f'ots stamp {report_file}')
+		subprocess.Popen(command, shell=True)
+				
+		report_file_ots	= report_file+'.ots'
+				
+		print(f'il report stampato sulla BLOCKCHAIN BITCOIN: \n {report_file_ots}')
+
+	elif event == 'Stampa finestra':
+		timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+		try:
+			
+			command = f'screencapture -w reports/{timestamp}.png'
+			subprocess.run(command, shell=True)
+				
+			sg.popup('Screenshot catturato e salvato come file PNG', title='Successo')
+	
+		except Exception as e:
+			sg.popup('Si è verificato un errore durante la cattura dello screenshot.', title='Errore')
+
+window.close()
